5b7c4836f4ad29f658cdd218b4222b3e51548fd8
Pawel <pawel80@gmail.com>
2025-10-23 22:09:35 +0200

    alarm_notification_v26

--- source/twilio_func_code_deploy.py
+++ source/twilio_func_code_deploy.py
@@ -19,7 +19,7 @@ def main():
                                            args.service_sid, args.func_sid, args.code_location)
     func_build_sid = tw_func_build(args.url, args.account_sid, args.auth_token, 
                                    args.service_sid, func_version_sid)
-    time.sleep(5)
+    time.sleep(10)
     tw_func_build_deploy(args.url, args.account_sid, args.auth_token, 
                          args.service_sid, args.env_sid, func_build_sid)
 
