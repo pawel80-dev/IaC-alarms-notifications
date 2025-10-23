9a532d4486833904ea0a2d6cc7eb3a19689376ac
Pawel <pawel80@gmail.com>
2025-10-23 20:15:39 +0200

    alarm_notification_v06

---
+++ source/twilio_env_var.py
@@ -0,0 +1,23 @@
+from twilio_api import tw_func_create_env_var
+# import logging
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("url", type=str, help="Twilio API URL")
+# parser.add_argument("code_url", type=str, help="Twilio Function code upload API URL")
+parser.add_argument("account_sid", type=str, help="Twilio account SID")
+parser.add_argument("auth_token", type=str, help="Twilio authentication token")
+parser.add_argument("service_sid", type=str, help="Twilio service SID")
+parser.add_argument("env_sid", type=str, help="Twilio environment SID")
+parser.add_argument("var_key", type=str, help="Twilio environment variable key")
+parser.add_argument("var_value", type=str, help="Twilio environment variable value")
+args = parser.parse_args()
+
+
+def main():
+    tw_func_create_env_var(args.url, args.account_sid, args.auth_token, args.service_sid, 
+                           args.env_sid, args.var_key, args.var_value)
+
+
+if __name__ == "__main__":
+    main()
