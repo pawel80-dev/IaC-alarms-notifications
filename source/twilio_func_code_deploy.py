210f3a99a76521ff828de70804a9fe5208381973
Pawel <pawel80@gmail.com>
2025-10-23 21:27:58 +0200

    alarm_notification_v20

---
+++ source/twilio_func_code_deploy.py
@@ -0,0 +1,25 @@
+from twilio_api import tw_func_code_upload, tw_func_build, tw_func_build_deploy
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("url", type=str, help="Twilio API URL")
+parser.add_argument("account_sid", type=str, help="Twilio account SID")
+parser.add_argument("auth_token", type=str, help="Twilio authentication token")
+parser.add_argument("service_sid", type=str, help="Twilio service SID")
+parser.add_argument("func_sid", type=str, help="Twilio function SID")
+parser.add_argument("env_sid", type=str, help="Twilio environment SID")
+# parser.add_argument("build_sid", type=str, help="Twilio build SID")
+args = parser.parse_args()
+
+
+def main():
+    func_version_sid = tw_func_code_upload(args.code_url, args.account_sid, args.auth_token, 
+                                           args.service_sid, args.func_sid, args.code_location)
+    func_build_sid = tw_func_build(args.url, args.account_sid, args.auth_token, 
+                                   args.service_sid, func_version_sid)
+    tw_func_build_deploy(args.url, args.account_sid, args.auth_token, 
+                         args.service_sid, args.env_sid, func_build_sid)
+
+
+if __name__ == "__main__":
+    main()
