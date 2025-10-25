11b81ab7a7e52fdb5a97227ad15f8eacce3c48c2
Pawel <pawel80@gmail.com>
2025-10-25 19:56:00 +0200

    alarm_notification_v35

---
+++ source/twilio_service_add.py
@@ -0,0 +1,19 @@
+from twilio_api import tw_service_create, tw_func_create
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("url", type=str, help="Twilio API URL")
+parser.add_argument("account_sid", type=str, help="Twilio account SID")
+parser.add_argument("auth_token", type=str, help="Twilio authentication token")
+parser.add_argument("service_name", type=str, help="Twilio service name")
+parser.add_argument("func_name", type=str, help="Twilio Function name")
+args = parser.parse_args()
+
+
+def main():
+    service_sid = tw_service_create(args.url, args.account_sid, args.auth_token, args.service_name)
+    tw_func_create(args.url, args.account_sid, args.auth_token, service_sid, args.func_name)
+
+
+if __name__ == "__main__":
+    main()
