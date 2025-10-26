e7e1efc31f94b7dc1cb2ce52962faea154017aed
Pawel <pawel80@gmail.com>
2025-10-26 14:47:36 +0100

    alarm_notification_v44

--- source/twilio_env_var.py
+++ source/twilio_env_var.py
@@ -1,4 +1,4 @@
-from twilio_api import tw_func_create_env_var
+from twilio_api import tw_func_env_vars_list, tw_func_create_env_var, tw_func_update_env_var
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -13,6 +13,16 @@ args = parser.parse_args()
 
 
 def main():
+    var_list = tw_func_env_vars_list(args.url, args.account_sid, args.auth_token, 
+                                     args.service_sid, args.env_sid)
+    # if variable already exists, it will be updated
+    for var in var_list:
+        if args.var_key == var["name"]:
+            tw_func_update_env_var(args.url, args.account_sid, args.auth_token, 
+                                   args.service_sid, args.env_sid, var["sid"], args.var_key, args.var_value)
+            exit(0)
+
+    # if variable doesn't exist, it will be created
     tw_func_create_env_var(args.url, args.account_sid, args.auth_token, args.service_sid, 
                            args.env_sid, args.var_key, args.var_value)
 
