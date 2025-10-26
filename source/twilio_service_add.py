0266b334ad02d967ee8a6ad0c287d6013998a506
Pawel <pawel80@gmail.com>
2025-10-26 13:55:45 +0100

    alarm_notification_v40

--- source/twilio_service_add.py
+++ source/twilio_service_add.py
@@ -1,4 +1,4 @@
-from twilio_api import tw_service_create, tw_func_create, tw_func_create_env
+from twilio_api import tw_service_list, tw_service_create, tw_func_create, tw_func_create_env
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -12,6 +12,14 @@ args = parser.parse_args()
 
 
 def main():
+    services_list = tw_service_list(args.url, args.account_sid, args.auth_token)
+    # if service already exists, add only new environment
+    for service in services_list:
+        if args.service_name == service["u_name"]:
+            tw_func_create_env(args.url, args.account_sid, args.auth_token, service["sid"], args.env)
+            exit(0)
+
+    # if service doesn't exist, it will be created together with Function and env
     service_sid = tw_service_create(args.url, args.account_sid, args.auth_token, args.service_name)
     tw_func_create(args.url, args.account_sid, args.auth_token, service_sid, args.func_name)
     tw_func_create_env(args.url, args.account_sid, args.auth_token, service_sid, args.env)
